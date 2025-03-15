ed5575bb76aeb607679fb3657ad628b05dc5b345
Your Name <you@example.com>
2025-03-15 15:23:41 +0100

    Pulizia del codice

--- src/it/unibs/pajc/server/Server.java
+++ src/it/unibs/pajc/server/Server.java
@@ -103,7 +103,7 @@ public class Server {
                 campoDiGioco.updatePhysics();
                 broadcastGameState();
             }
-        }, 0, 16); // circa 60 FPS
+        }, 0, 33); // circa 30 FPS
     }
 
     private synchronized void checkAndStartGame() {
